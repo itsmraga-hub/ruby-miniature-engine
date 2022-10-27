fcea3b27d6609cf021dbdfd22d07c17608ec377d
Raga <ragawilliam570@gmail.com>
2022-10-27 11:27:13 +0300

    Add my_enumerable module file

---
+++ my_enumerable.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# MyEnumerable module implementing metjods all, any and filter
+module MyEnumerable
+  def any?
+    each { |item| return true if yield item }
+    false
+  end
+
+  def all?
+    each { |item| return false unless yield item }
+    true
+  end
+
+  def filter
+    result = []
+    each { |item| result.push(item) if yield item }
+    result
+  end
+end
